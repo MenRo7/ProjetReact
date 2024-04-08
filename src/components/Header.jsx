881d4fcc52c5e992f6fe336ed8128235b8905de1
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-08 14:53:27 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Header() {
     return (
-        <div>
+        <header>
             <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo" />
-            <ul>
-                <li><a href="/">Accueil</a></li>
-            </ul>
-        </div>
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
