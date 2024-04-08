956dd13a8423d3e00c1cdf23296aca5a34ddc1ce
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-08 14:53:59 +0200

    footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,11 @@ function Header() {
     return (
         <div>
             <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo" />
-            <ul>
-                <li><a href="/">Accueil</a></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
