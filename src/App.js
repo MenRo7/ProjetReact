b244bce118a6b44be8b85630af287e8e02b94122
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-08 16:35:48 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
           price="15" 
           img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
         />
+      </main>
       <Footer/>
     </>
   );
