4b4a9f8d0a34178db447f9c56d3ba0d5aa767ba0
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 15:33:41 +0200

    routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,25 +9,25 @@ const Home = () => {
             <Container>
                 <Row className="dish-container">
                     <Col>
-                    <Dish
-                        name="Tacos à l'unité"
-                        price="3"
-                        img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                    />
+                        <Dish
+                            name="Tacos à l'unité"
+                            price="3"
+                            img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+                        />
                     </Col>
                     <Col>
-                    <Dish
-                        name="Enchiladas"
-                        price="12"
-                        img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                    />
+                        <Dish
+                            name="Enchiladas"
+                            price="12"
+                            img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+                        />
                     </Col>
                     <Col>
-                    <Dish
-                        name="Mole Poblano"
-                        price="15"
-                        img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                    />
+                        <Dish
+                            name="Mole Poblano"
+                            price="15"
+                            img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+                        />
                     </Col>
                 </Row>
             </Container>
