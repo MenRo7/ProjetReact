3cdcf3beecbd31621eb810d920abf85ce26abf98
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 16:53:50 +0200

    links

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -3,7 +3,8 @@ import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../assets/style/dish.css';
 
-const Dish = ({ name, price, img, to }) => {
+const Dish = ({ name, price, img, slug }) => {
+    const to = `/plat/${slug}`;
     return (
         <Link to={to} className="dish-link">
             <Card className="dish-card">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                         name="Tacos à l'unité"
                         price="3"
                         img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                        to="/tacos"
+                        slug="tacos"
                     />
                 </Col>
                 <Col>
@@ -19,7 +19,7 @@ const Home = () => {
                         name="Enchiladas"
                         price="12"
                         img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                        to="/enchiladas"
+                        slug="enchiladas"
                     />
                 </Col>
                 <Col>
@@ -27,7 +27,7 @@ const Home = () => {
                         name="Mole Poblano"
                         price="15"
                         img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                        to="/mole-poblano"
+                        slug="mole-poblano"
                     />
                 </Col>
             </Row>
