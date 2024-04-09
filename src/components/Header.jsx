3c08a7c750869034cfd4fae20dfb7ef52239caf3
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 12:11:22 +0200

    asset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, NavLink, Container } from 'react-bootstrap';
+import logo from '../assets/images/logo.webp';
 import '../assets/style/header.css';
 
 function Header() {
@@ -8,7 +9,7 @@ function Header() {
             <Navbar>
                 <Container>
                     <Navbar.Brand>
-                        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo" className="logo" />
+                        <img src={logo} alt="Logo" className="logo" />
                     </Navbar.Brand>
                     <Navbar.Collapse className="justify-content-end">
                         <Nav>
