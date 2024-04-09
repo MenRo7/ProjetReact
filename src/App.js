c34558b697d464c7ef269ce4471cc98c48560d05
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 16:27:48 +0200

    404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './assets/style/App.css';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import DishDetails from './pages/DishDetails';
+import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 				<Route element={<Layout />}>
 					<Route index element={<Home />} />
 					<Route path="plat/:slug" element={<DishDetails />} />
+					<Route path="*" element={<NotFound />} />
 				</Route>
 			</Routes>
 		</>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const NotFound = () => {
+    return (
+        <Container>
+            <Row>
+                <h1>Page non trouvée</h1>
+            </Row>
+        </Container>
+    );
+}
+
+export default NotFound;
