978554c502dddca2940148da17e7d582514394eb
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 15:27:06 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './assets/style/App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<main>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route index element={<Home />} />
+					</Route>
+				</Routes>
+			</main>
+		</>
+	);
 }
 
 export default App;
