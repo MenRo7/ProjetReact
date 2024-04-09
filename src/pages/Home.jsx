13817741bd7380d868502331dc8120d030fef065
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-09 16:38:56 +0200

    links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Dish from '../components/Dish';
-
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -9,25 +9,31 @@ const Home = () => {
             <Container>
                 <Row className="dish-container">
                     <Col>
-                        <Dish
-                            name="Tacos à l'unité"
-                            price="3"
-                            img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                        />
+                        <Link to="/tacos">
+                            <Dish
+                                name="Tacos à l'unité"
+                                price="3"
+                                img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+                            />
+                        </Link>
                     </Col>
                     <Col>
-                        <Dish
-                            name="Enchiladas"
-                            price="12"
-                            img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                        />
+                        <Link to="/enchiladas">
+                            <Dish
+                                name="Enchiladas"
+                                price="12"
+                                img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+                            />
+                        </Link>
                     </Col>
                     <Col>
-                        <Dish
-                            name="Mole Poblano"
-                            price="15"
-                            img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                        />
+                        <Link to="/mole-poblano">
+                            <Dish
+                                name="Mole Poblano"
+                                price="15"
+                                img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+                            />
+                        </Link>
                     </Col>
                 </Row>
             </Container>
