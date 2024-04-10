c1d4800886ba691d4d209cff8b47db929b6c51b8
Romain Garnier <romain.garnier1@etudiant.univ-reims.fr>
2024-04-10 11:40:42 +0200

    map & key

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,36 +3,43 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Dish from '../components/Dish';
 
 const Home = () => {
+    const dishes = [
+        {
+            name: "Tacos à l'unité",
+            price: "3",
+            img: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+            slug: "tacos"
+        },
+        {
+            name: "Enchiladas",
+            price: "12",
+            img: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+            slug: "enchiladas"
+        },
+        {
+            name: "Mole Poblano",
+            price: "15",
+            img: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+            slug: "mole-poblano"
+        }
+    ];
+
     return (
         <Container>
             <Row className="dish-container">
-                <Col>
-                    <Dish
-                        name="Tacos à l'unité"
-                        price="3"
-                        img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                        slug="tacos"
-                    />
-                </Col>
-                <Col>
-                    <Dish
-                        name="Enchiladas"
-                        price="12"
-                        img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                        slug="enchiladas"
-                    />
-                </Col>
-                <Col>
-                    <Dish
-                        name="Mole Poblano"
-                        price="15"
-                        img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                        slug="mole-poblano"
-                    />
-                </Col>
+                {dishes.map((dish, index) => (
+                    <Col key={index}>
+                        <Dish
+                            name={dish.name}
+                            price={dish.price}
+                            img={dish.img}
+                            slug={dish.slug}
+                        />
+                    </Col>
+                ))}
             </Row>
         </Container>
-    )
+    );
 }
 
 export default Home;
